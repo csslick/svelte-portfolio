d65284f8bd97b60a507aba5b535311dc3125a522
csslick <digitone@naver.com>
2024-02-20 00:12:25 +0900

    s2-5 routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,27 @@
 <script>
+  import {Router, Route, Link} from'svelte-routing';
   import Title from './lib/components/Title.svelte';
   import Portfolio from './lib/components/Portfolio.svelte';
   import About from './lib/components/About.svelte';
 
   let title = 'Hello,<br> I\'m Doolee';
+  export let url = '';
 </script>
 
-<nav>
-  <a href="#home">home</a>
-  <a href="#portfolio">portfolio</a>
-</nav>
-<Title {title} />
-<Portfolio />
-<About />
+<Router {url}>
+  <nav>
+    <Link to="/">home</Link>
+    <Link to="/portfolio">portfolio</Link>
+    <Link to="/about">about</Link>
+  </nav>
+
+  <div>
+    <Route path="/" component={Title} {title} />
+    <Route path="/portfolio" component={Portfolio} />
+    <Route path="/about" component={About} />
+  </div>
+</Router>
+
 
 
 
